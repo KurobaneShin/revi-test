bf605abfef22e3650b7cc2368d80855608211ccf
icaro <darkflamemaster663@gmail.com>
2024-09-08 13:48:38 -0300

    delete monster and readme

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,7 +15,6 @@ import { Card, CardContent } from "~/components/ui/card";
 import {
   ClientActionFunctionArgs,
   Form,
-  Link,
   useActionData,
   useLoaderData,
   useNavigate,
@@ -25,6 +24,7 @@ import { z } from "zod";
 import { useForm } from "@conform-to/react";
 import { useState } from "react";
 import { cn } from "~/lib/utils";
+import { Trash } from "lucide-react";
 
 const monsterSchema = z.object({
   id: z.string().uuid(),
@@ -50,6 +50,18 @@ export const clientAction = async (
   { request, serverAction }: ClientActionFunctionArgs,
 ) => {
   const formData = await request.clone().formData();
+
+  if (request.method === "DELETE") {
+    const id = formData.get("id");
+    if (!id) return await serverAction<typeof action>();
+    const monsters = monstersSchema.parse(
+      JSON.parse(sessionStorage.getItem("monsters") || "[]"),
+    );
+    monsters.splice(monsters.findIndex((m) => m.id === id), 1);
+    sessionStorage.setItem("monsters", JSON.stringify(monsters));
+    return await serverAction<typeof action>();
+  }
+
   const submission = parseWithZod(formData, { schema: monsterSchema });
 
   if (submission.status !== "success") {
@@ -69,15 +81,10 @@ export default function Index() {
   const [selectedMonsters, setSelectedMonsters] = useState<string[]>([]);
   const navigate = useNavigate();
   const [form, fields] = useForm({
-    // Sync the result of last submission
     lastResult,
-
-    // Reuse the validation logic on the client
     onValidate({ formData }) {
       return parseWithZod(formData, { schema: monsterSchema });
     },
-
-    // Validate the form on blur event triggered
     shouldValidate: "onBlur",
     shouldRevalidate: "onInput",
   });
@@ -200,39 +207,48 @@ export default function Index() {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {monsters.map(({ name, attack, defense, speed, hp, image, id }) => (
-              <Card
-                className={cn(
-                  "cursor-pointer",
-                  `${selectedMonsters.includes(id) ? "bg-blue-500" : ""}`,
-                )}
-                key={name}
-                onClick={() =>
-                  selectMonstersToFight(id)}
-              >
-                <img
-                  src={image}
-                  width={200}
-                  height={200}
-                  alt="Monster"
-                  className="rounded-t-lg object-cover w-full aspect-square"
-                />
-                <CardContent className="p-4">
-                  <h3 className="text-lg font-bold">{name}</h3>
-                  <div className="grid grid-cols-2 gap-2 mt-2">
-                    <div>
-                      <span className="font-medium">Attack:</span> {attack}
+              <Card key={id} className="space-y-2">
+                <Card
+                  className={cn(
+                    "cursor-pointer",
+                    `${selectedMonsters.includes(id) ? "bg-blue-500" : ""}`,
+                  )}
+                  key={name}
+                  onClick={() =>
+                    selectMonstersToFight(id)}
+                >
+                  <img
+                    src={image}
+                    width={200}
+                    height={200}
+                    alt="Monster"
+                    className="rounded-t-lg object-cover w-full aspect-square"
+                  />
+                  <CardContent className="p-4">
+                    <h3 className="text-lg font-bold">{name}</h3>
+                    <div className="grid grid-cols-2 gap-2 mt-2">
+                      <div>
+                        <span className="font-medium">Attack:</span> {attack}
+                      </div>
+                      <div>
+                        <span className="font-medium">Defense:</span> {defense}
+                      </div>
+                      <div>
+                        <span className="font-medium">Speed:</span> {speed}
+                      </div>
+                      <div>
+                        <span className="font-medium">HP:</span> {hp}
+                      </div>
                     </div>
-                    <div>
-                      <span className="font-medium">Defense:</span> {defense}
-                    </div>
-                    <div>
-                      <span className="font-medium">Speed:</span> {speed}
-                    </div>
-                    <div>
-                      <span className="font-medium">HP:</span> {hp}
-                    </div>
-                  </div>
-                </CardContent>
+                  </CardContent>
+                </Card>
+
+                <Form method="delete">
+                  <input type="hidden" name="id" value={id} />
+                  <button type="submit">
+                    <Trash className="text-red-500 hover:text-red-700" />
+                  </button>
+                </Form>
               </Card>
             ))}
           </div>
